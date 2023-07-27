4a44d1988993047b22d5c917193b6785cecd9c22
Stephen.Havig <stephen.havig@gmail.com>
2023-07-27 14:21:51 -0700

    setting up backend for connecting with postgres database

--- src/pages/budget.tsx
+++ src/pages/budget.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from 'next/link';
 import Navbar from './navbar/navbar';
 
 const BudgetPage: React.FC = () => {
+
+    const [budget, setBudget] = useState({});
+
+    useEffect(() => {
+        try{
+            const loadBudget = async () => {
+                await fetch(`/api/obtainBudgetData`)
+                .then(data => data.json())
+                .then(data => {
+                    setBudget(data)
+                })
+            }
+            loadBudget();
+        } catch {
+
+        }
+    }, [])
+
   return (
     <div>
         <div>
