fd20722d96cb2f34af24673ee43fec32e96ca619
Stephen.Havig <stephen.havig@gmail.com>
2023-08-14 14:12:31 -0700

    connecting prisma to frontend

--- src/pages/api/obtainBudgetData.tsx
+++ src/pages/api/obtainBudgetData.tsx
@@ -1,17 +1,17 @@
-import conn from './db';
-import {NextApiRequest, NextApiResponse} from 'next'
+// import conn from './db';
+// import {NextApiRequest, NextApiResponse} from 'next'
 
-export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
-    try {
-        conn.connect()
-        .then(() => {
-            const data: {} = conn.query(`SELECT * FROM public.${process.env.PGSQL_DATABASE}`)
-        })
-        .then((data) => {
-            res.locals = data
-        })
-    } catch {
+// export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+//     try {
+//         conn.connect()
+//         .then(() => {
+//             const data: {} = conn.query(`SELECT * FROM public.${process.env.PGSQL_DATABASE}`)
+//         })
+//         .then((data) => {
+//             res.locals = data
+//         })
+//     } catch {
 
-    }
-    res.status(200).json({ name: 'John Doe' })
-  }
+//     }
+//     res.status(200).json({ name: 'John Doe' })
+//   }
